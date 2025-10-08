a2d322c2e73c2853b3a6128cafd4764b7f0de7a0
ameliazsabrina <152615423+ameliazsabrina@users.noreply.github.com>
2025-10-08 09:16:11 +0700

    feat: change functions

--- app/src/main/java/com/example/testing/MainActivity.kt
+++ app/src/main/java/com/example/testing/MainActivity.kt
@@ -4,16 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.testing.ui.theme.TestingTheme
 
@@ -23,35 +26,51 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TestingTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Amel",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                KartuHariRaya(
+                    message = "Selamat Hari Rabu",
+                    name = "Amelia",
+                    from = "Fufufafa"
+                )
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = modifier.fillMaxSize()
+fun KartuHariRaya(message: String, name: String, from: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+            .fillMaxSize()
+//            .padding(16.dp)
+            .background(color = androidx.compose.ui.graphics.Color.Yellow)
     ) {
         Text(
-            text = "Assalamualaikum $name!",
+            text = "$message $name!",
+            fontSize = 32.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 16.dp)
+        )
+        Text(
+            text = "From: $from",
             fontSize = 24.sp,
-            fontWeight = FontWeight.Bold
+            fontStyle = androidx.compose.ui.text.font.FontStyle.Italic,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
         )
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun KartuHariRayaPreview() {
     TestingTheme {
-        Greeting("Android")
+        KartuHariRaya(
+            message = "Selamat Hari Rabu!",
+            name = "Amelia",
+            from = "Fufufafa"
+        )
     }
 }
